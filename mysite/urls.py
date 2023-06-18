9b518a6af8aa8f5135b5241c7cb14d90703709e7
kemas <kemas.m3@gmail.com>
2023-06-18 17:37:42 +0700

    add urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -30,7 +30,7 @@ urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    path(r'',include(puput_urls)),
+    path(r'', include(puput_urls)),
     path("", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
